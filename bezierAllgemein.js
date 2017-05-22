4114910f720008e3d70d8711548416a11efc6f4a
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2017-05-22 13:38:17 +0200

    Added helper file

---
+++ bezierAllgemein.js
@@ -0,0 +1,23 @@
+function P(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+var inputArray = new Array();
+inputArray.push(new P(5, 3));
+inputArray.push(new P(7, 9));
+inputArray.push(new P(1, 8));
+
+var outputArray = new Array();
+
+
+function bezier(outputArray, tiefe) {
+    for (var i = 0; i < inputArray.length - 1; i++) {
+        outputArray.push(new P(0.5 * (inputArray[i].x + inputArray[i + 1].x), 0.5 * (inputArray[i].y + inputArray[i + 1].y)));
+    }
+}
+
+
+
+
+console.log(outputArray);
