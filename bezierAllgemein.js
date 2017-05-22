7bd090098ace58527624040bd897228a72c2069b
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2017-05-22 15:58:57 +0200

    Blubb

--- bezierAllgemein.js
+++ bezierAllgemein.js
@@ -8,7 +8,7 @@ inputArray.push(new P(5, 3));
 inputArray.push(new P(7, 9));
 inputArray.push(new P(1, 8));
 inputArray.push(new P(3, 7));
-inputArray.push(new P(7, 7));
+//inputArray.push(new P(7, 7));
 
 function bezier(points, tiefe) {
     var outputArray = new Array();
@@ -28,17 +28,20 @@ function bezier2(points, depth) {
         for (var i = 0; i < points.length-1; i++) {
             var layerPoints = new Array();
             for (j = counter; j < points.length; j++) {
+                var firstEntry = 0;
                 if (counter == 1) {
                     layerPoints.push(new P(0.5 * (points[j-1].x + points[j].x), 0.5 * (points[j-1].y + points[j].y)));
                 }
                 else {
-                    //layerPoints.push();
+                    //console.log(i, j);
+                    layerPoints.push(new P(0.5 * (myPoints[counter-2][firstEntry].x + myPoints[counter-2][firstEntry+1].x), 0.5 * (myPoints[counter-2][firstEntry].y + myPoints[counter-2][firstEntry+1].y)));
+                    firstEntry++;
                 }
             }
             counter++;
             myPoints.push(layerPoints);
         }
-        console.log(myPoints[0]);
+        console.log(myPoints);
     }
 }
 
