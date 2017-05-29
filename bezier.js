104a0639e99e151ca2c63b1688d0b5878abde05e
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-05-29 14:25:23 +0200

    aktueller stand

--- bezier.js
+++ bezier.js
@@ -10,20 +10,21 @@ function getRandomPoint(width, height) {
         Math.floor(Math.random() * height));
 }
 
-var width = 600;
+var inputArray = [];
+let myReturnArray;
+var width  = 600;
 var height = 600;
 var max_bezier_depth = 5;
-var num_points = 3;
+var num_points = 4;
 var CP = Array(num_points);
 var line_width = 2;
 var point_size = 3;
 var back_color = '#303030';
 var line_color = '#ff1010';
 var point_color = '#40f040';
-var tangent_color = '#1010f0';
 
-function draw() {
-    for (var i = 0; i < num_points; i++) {
+function draw () {
+    for (var i=0; i<num_points; i++) {
         CP[i] = getRandomPoint(width, height);
     }
     if (ctx) {
@@ -31,97 +32,88 @@ function draw() {
         ctx.fillRect(0, 0, width, height);
         ctx.lineWidth = line_width;
         ctx.strokeStyle = line_color;
-        bezier(CP);
 
-        for (var i = 0; i < num_points; i++) {
+        myReturnArray = bezierAllgemein(CP, max_bezier_depth);
+
+        for (var i=0; i<num_points; i++) {
             point(CP[i]);
         }
     }
 }
 
-function point(P) {
+function point (P)
+{
     ctx.fillStyle = point_color;
-    ctx.fillRect(P.x - point_size / 2, P.y - point_size / 2, point_size, point_size);
+    ctx.fillRect(P.x-point_size/2,P.y-point_size/2,point_size,point_size);
 }
-function line(P0, P1) {
+
+function line (P0, P1) {
     ctx.beginPath();
     ctx.moveTo(P0.x, P0.y);
     ctx.lineTo(P1.x, P1.y);
     ctx.stroke();
 }
 
-function tangentLine(P0, P1) {
-    ctx.beginPath();
-    ctx.moveTo(P0.x, P0.y);
-    ctx.lineTo(P1.x, P1.y);
-    ctx.stroke();
-}
-
-function bezier3(P0, P1, P2, tiefe) {
-    if (tiefe == 0) line(P0, P2);
-    else {
-        var P01 = new P(0.5 * (P0.x + P1.x), 0.5 * (P0.y + P1.y));
-        var P12 = new P(0.5 * (P1.x + P2.x), 0.5 * (P1.y + P2.y));
-
-        var P012 = new P(0.5 * (P01.x + P12.x), 0.5 * (P01.y + P12.y));
-
-        bezier3(P0, P01, P012, tiefe - 1);
-        bezier3(P012, P12, P2, tiefe - 1);
-    }
-}
-function bezier4(P0, P1, P2, P3, tiefe) {
+function bezier4 (P0, P1, P2, P3, tiefe) {
     if (tiefe == 0) line(P0, P3);
     else {
-        var P01 = new P(0.5 * (P0.x + P1.x), 0.5 * (P0.y + P1.y));
-        var P12 = new P(0.5 * (P1.x + P2.x), 0.5 * (P1.y + P2.y));
-        var P23 = new P(0.5 * (P2.x + P3.x), 0.5 * (P2.y + P3.y));
-        tangentLine(P01, P12);
-        tangentLine(P12, P23);
+        var P01   = new P(0.5*(P0.x+P1.x), 0.5*(P0.y+P1.y));
+        var P12   = new P(0.5*(P1.x+P2.x), 0.5*(P1.y+P2.y));
+        var P23   = new P(0.5*(P2.x+P3.x), 0.5*(P2.y+P3.y));
 
-        var P012 = new P(0.5 * (P01.x + P12.x), 0.5 * (P01.y + P12.y));
-        var P123 = new P(0.5 * (P12.x + P23.x), 0.5 * (P12.y + P23.y));
-        tangentLine(P012, P123);
+        var P012  = new P(0.5*(P01.x+P12.x), 0.5*(P01.y+P12.y));
+        var P123  = new P(0.5*(P12.x+P23.x), 0.5*(P12.y+P23.y));
 
+        var P0123 = new P(0.5*(P012.x+P123.x), 0.5*(P012.y+P123.y));
 
-        var P0123 = new P(0.5 * (P012.x + P123.x), 0.5 * (P012.y + P123.y));
-
-        bezier4(P0, P01, P012, P0123, tiefe - 1);
-        bezier4(P0123, P123, P23, P3, tiefe - 1);
+        bezier4(P0, P01, P012, P0123, tiefe-1);
+        bezier4(P0123, P123, P23, P3, tiefe-1);
     }
 }
 
-function bezier5(P0, P1, P2, P3, P4, tiefe) {
-    if (tiefe == 0) line(P0, P4);
-    else {
-        var P01 = new P(0.5 * (P0.x + P1.x), 0.5 * (P0.y + P1.y));
-        var P12 = new P(0.5 * (P1.x + P2.x), 0.5 * (P1.y + P2.y));
-        var P23 = new P(0.5 * (P2.x + P3.x), 0.5 * (P2.y + P3.y));
-        var P34 = new P(0.5 * (P3.x + P4.x), 0.5 * (P3.y + P4.y));
-
-        var P012 = new P(0.5 * (P01.x + P12.x), 0.5 * (P01.y + P12.y));
-        var P123 = new P(0.5 * (P12.x + P23.x), 0.5 * (P12.y + P23.y));
-        var P234 = new P(0.5 * (P23.x + P34.x), 0.5 * (P23.y + P34.y));
-
-        var P0123 = new P(0.5 * (P012.x + P123.x), 0.5 * (P012.y + P123.y));
-        var P1234 = new P(0.5 * (P123.x + P234.x), 0.5 * (P123.y + P234.y));
-
-        var P01234 = new P(0.5 * (P0123.x + P1234.x), 0.5 * (P0123.y + P1234.y));
-
-
-        bezier5(P0, P01, P012, P0123, P01234, tiefe - 1);
-        bezier5(P01234, P1234, P234, P34, P4, tiefe - 1);
-    }
-}
-
-function bezier(points) {
-
-    if (points.length == 3) {
-        bezier3(points[0], points[1], points[2], max_bezier_depth);
-    }
+function bezier (points) {
     if (points.length == 4) {
-        bezier4(points[0], points[1], points[2], points[3], max_bezier_depth);
+        bezier4 (points[0], points[1], points[2], points[3], max_bezier_depth);
     }
-    if (points.length == 5) {
-        bezier5(points[0], points[1], points[2], points[3], points[4], max_bezier_depth);
+}
+
+
+function bezierAllgemein(points, depth) {
+    const myPoints = [];
+    const myReturnArray1 = [];
+    const myReturnArray2 = [];
+
+    let counter = 1;
+    if (depth !== 0) {
+        for (let i = 0; i < points.length-1; i++) {
+            const layerPoints = [];
+            for (let j = counter; j < points.length; j++) {
+                let firstEntry = 0;
+                if (counter === 1) {
+                    layerPoints.push(new P(0.5 * (points[j-1].x + points[j].x), 0.5 * (points[j-1].y + points[j].y)));
+                }
+                else {
+                    //console.log(i, j);
+                    layerPoints.push(new P(0.5 * (myPoints[counter-2][firstEntry].x + myPoints[counter-2][firstEntry+1].x),
+                        0.5 * (myPoints[counter-2][firstEntry].y + myPoints[counter-2][firstEntry+1].y)));
+                    firstEntry++;
+                }
+            }
+            counter++;
+            myPoints.push(layerPoints);
+        }
+        myReturnArray1.push(points[0]);
+        for (let i = 0; i < myPoints.length; i++) {
+            myReturnArray1.push(myPoints[i][0])
+        }
+        for (let j = myPoints.length-1; j >= 0; j--){
+            myReturnArray2.push(myPoints[j][myPoints[j].length-1]);
+        }
+        myReturnArray2.push(points[points.length-1]);
+
+        bezierAllgemein(myReturnArray1, depth-1);
+        bezierAllgemein(myReturnArray2, depth-1);
+    } else {
+        return points;
     }
 }
--- bezierAllgemein.js
+++ bezierAllgemein.js
@@ -3,17 +3,12 @@ function P(x, y) {
     this.y = y;
 }
 
-var inputArray = new Array();
-inputArray.push(new P(5, 3));
-inputArray.push(new P(7, 9));
-inputArray.push(new P(1, 8));
-inputArray.push(new P(3, 7));
-//inputArray.push(new P(7, 7));
+var inputArray = [];
 
 function bezier(points, tiefe) {
-    var outputArray = new Array();
+    const outputArray = [];
     if (tiefe !== 0) {
-        for (var i = 0; i < points.length - 1; i++) {
+        for (let i = 0; i < points.length - 1; i++) {
             outputArray.push(new P(0.5 * (points[i].x + points[i + 1].x), 0.5 * (points[i].y + points[i + 1].y)));
         }
         bezier(outputArray, tiefe-1);
@@ -21,20 +16,21 @@ function bezier(points, tiefe) {
     return outputArray;
 }
 
-function bezier2(points, depth) {
-    var myPoints = new Array();
-    var counter = 1;
+function bezierAllgemein(points, depth) {
+    const myPoints = [];
+    let counter = 1;
     if (depth !== 0) {
-        for (var i = 0; i < points.length-1; i++) {
-            var layerPoints = new Array();
-            for (j = counter; j < points.length; j++) {
-                var firstEntry = 0;
-                if (counter == 1) {
+        for (let i = 0; i < points.length-1; i++) {
+            const layerPoints = [];
+            for (let j = counter; j < points.length; j++) {
+                let firstEntry = 0;
+                if (counter === 1) {
                     layerPoints.push(new P(0.5 * (points[j-1].x + points[j].x), 0.5 * (points[j-1].y + points[j].y)));
                 }
                 else {
                     //console.log(i, j);
-                    layerPoints.push(new P(0.5 * (myPoints[counter-2][firstEntry].x + myPoints[counter-2][firstEntry+1].x), 0.5 * (myPoints[counter-2][firstEntry].y + myPoints[counter-2][firstEntry+1].y)));
+                    layerPoints.push(new P(0.5 * (myPoints[counter-2][firstEntry].x + myPoints[counter-2][firstEntry+1].x),
+                        0.5 * (myPoints[counter-2][firstEntry].y + myPoints[counter-2][firstEntry+1].y)));
                     firstEntry++;
                 }
             }
@@ -45,4 +41,4 @@ function bezier2(points, depth) {
     }
 }
 
-bezier2(inputArray, 5);
+bezierAllgemein(inputArray, 5);
